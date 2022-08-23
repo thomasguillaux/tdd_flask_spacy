4ab18853dc83e10a1a39d7bd0ca8cc8e1617a4a3
thomasguillaux <thomasguillaux@gmail.com>
2022-08-23 19:13:01 +0200

    feature: ner client completed

---
+++ ner_client.py
@@ -0,0 +1,20 @@
+class NamedEntityClient:
+    def __init__(self, model):
+        self.model = model
+
+    def get_ents(self, sentence):
+        doc = self.model(sentence)
+        entities = [{'ent' : ent.text, 'label': self.map_label(ent.label_)} for ent in doc.ents]
+        return {'ents': entities, 'html': ''}
+
+    @staticmethod
+    def map_label(label):
+        label_map = {
+            'PERSON'    : 'Person',
+            'NORP'      : 'Group',
+            'GPE'       : 'Location',
+            'LANGUAGE'  : 'Language'
+        }
+
+        return label_map.get(label)
+            
